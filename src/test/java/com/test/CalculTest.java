76ad2494d64aa0b6809c690f5c50583c590c141c
Papa Alioune <alinepape94@gmail.com>
2019-11-23 12:54:24 +0000

    second commit

--- src/test/java/com/test/CalculTest.java
+++ src/test/java/com/test/CalculTest.java
@@ -4,16 +4,27 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.ucad.Calcul;
+
 public class CalculTest {
+	Calcul calc = new Calcul();
 
 	@Test
 	public void testAddition() {
-		fail("Not yet implemented");
+		assertEquals("bon", calc.addition(23, 43));
 	}
 
 	@Test
 	public void testMultiplication() {
+		
 		fail("Not yet implemented");
 	}
+	@Test
+	public void testDouble()
+	{
+		
+	}
+	
+
 
 }
